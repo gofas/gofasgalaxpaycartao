41c5f038d7e3ef0520f4dd1ab78cfe67163675f2
Mauricio Gofas <mauricio@gofas.net>
2022-08-18 07:55:49 -0300

    v1.0.0

--- modules/gateways/gofasgalaxpaycartao.php
+++ modules/gateways/gofasgalaxpaycartao.php
@@ -5,6 +5,6 @@
  * @see			https://gofas.net/?p=14128
  * @license		https://gofas.net/?p=9340
  * @support		https://gofas.net/?p=12349
- * @version		0.1.0
+ * @version		1.0.0
  */
 require __DIR__.'/gofasgalaxpaycartao/index.php';
